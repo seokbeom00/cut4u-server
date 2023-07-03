213246a83c2c27ddc644eb681ecfd7744c45abfc
Yoo TaeSeung <remicon99@naver.com>
2023-07-03 21:25:34 +0900

    init project

---
+++ src/main/java/antmanclub/cut4userver/Cut4uServerApplication.java
@@ -0,0 +1,13 @@
+package antmanclub.cut4userver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cut4uServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cut4uServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/antmanclub/cut4userver/Cut4uServerApplicationTests.java
@@ -0,0 +1,13 @@
+package antmanclub.cut4userver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cut4uServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
