43129f3b491d28ae32d8c60c57d1a1cd876867f8
Yoo TaeSeung <remicon99@naver.com>
2023-08-04 16:49:33 +0900

    Update AwsUpload.java

--- src/main/java/antmanclub/cut4userver/aws/AwsUpload.java
+++ src/main/java/antmanclub/cut4userver/aws/AwsUpload.java
@@ -17,6 +17,7 @@ public class AwsUpload {
     private String bucket;
 
     private final AmazonS3 amazonS3;
+    // 아닌데용 전데용
 
     public String upload(MultipartFile multipartFile, String dirName) throws IOException {
         String s3FileName = dirName + "/" + UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
