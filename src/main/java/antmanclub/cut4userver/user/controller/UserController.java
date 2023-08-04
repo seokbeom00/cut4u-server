6ddf2938e6aa2e6d0c11ad700da76c96d4385f32
HongGit <soojam@konkuk.ac.kr>
2023-08-04 18:49:53 +0900

    유태승 로그인 주석

--- src/main/java/antmanclub/cut4userver/user/controller/UserController.java
+++ src/main/java/antmanclub/cut4userver/user/controller/UserController.java
@@ -6,60 +6,51 @@ import antmanclub.cut4userver.user.dto.LoginDto;
 import antmanclub.cut4userver.user.dto.UserDto;
 import antmanclub.cut4userver.user.service.UserServiceImpl;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserServiceImpl userService;
     private final AwsUpload awsUpload;
 
+//    @GetMapping("/")
+//    public String home(){
+//        return "home";
+//    }
+//
+//    @GetMapping("/join")
+//    public String joinForm(){
+//        return "join";
+//    }
+//
+//    @PostMapping("/join")
+//    public String join(JoinDto joinDto){
+//        userService.join(joinDto);
+//
+//        return "redirect:/";
+//    }
+//
+//    @GetMapping("/login")
+//    public String loginForm(){
+//        return "login";
+//    }
+//
+//    @PostMapping("/login")
+//    public String login(LoginDto loginDto){
+//        userService.login(loginDto);
+//
+//        return "redirect:/";
+//    }
 
-    @Autowired
-    public UserController(UserServiceImpl userService, AwsUpload awsUpload) {
-        this.userService = userService;
-        this.awsUpload = awsUpload;
-    }
-
-    @GetMapping("/")
-    public String home(){
-        return "home";
-    }
-
-    @GetMapping("/join")
-    public String joinForm(){
-        return "join";
-    }
-
-    @PostMapping("/join")
-    public String join(JoinDto joinDto){
-        userService.join(joinDto);
-
-        return "redirect:/";
-    }
-
-    @GetMapping("/login")
-    public String loginForm(){
-        return "login";
-    }
-
-    @PostMapping("/login")
-    public String login(LoginDto loginDto){
-        userService.login(loginDto);
-
-        return "redirect:/";
-    }
-
-    @ResponseBody   // Long 타입을 리턴하고 싶은 경우 붙여야 함 (Long - 객체)
     @PostMapping(value="/user/edit",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public String saveDiary(HttpServletRequest request, @RequestParam(value="image") MultipartFile image, UserDto userDto) throws IOException {
         String imgSrc = awsUpload.upload(image, "image");
